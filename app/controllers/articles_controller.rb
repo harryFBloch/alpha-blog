83e41eb6942f48a8c60e830857341267461b402a
rails practice <phishphace00@yahoo.com>
2017-12-05 20:50:57 +0000

    fix user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   def create
     #render plaifasdfn: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
