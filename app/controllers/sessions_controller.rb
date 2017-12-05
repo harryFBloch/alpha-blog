4f8ef9ef493a72cac05626999af5fe3631ab3401
rails practice <phishphace00@yahoo.com>
2017-12-05 18:30:31 +0000

    added login page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  
+  
+  def new
+    
+  end
+  
+  def create
+    #render 'new'
+    debugger
+  end
+  
+  def destroy
+    
+  end
+  
+  
+end
