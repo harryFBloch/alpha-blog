150f76808b71596852540b0ab025853a185e3e83
rails practice <phishphace00@yahoo.com>
2017-12-05 22:27:42 +0000

    modified user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,28 +5,19 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user, :logged_in?
   
-def current_user
-
-@current_user ||= User.find(session[:user_id]) if session[:user_id]
-
-end
-
-def logged_in?
-
-!!current_user
-
-end
-
-def require_user
-
-if !logged_in?
-
-flash[:danger] = "You must be logged in to perform that action"
-
-redirect_to root_path
-
-end
-
-end
-
+  def current_user
+    @current_user ||= User.find(session[:user_id])if session[:user_id]
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must Be Logged In To Perfom That Action"
+      redirect_to root_path
+    end
+  end
+  
 end
