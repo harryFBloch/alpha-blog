19b1bd18ed2b41b6a099b293028ae19e74cd4c1c
rails practice <phishphace00@yahoo.com>
2017-12-06 19:51:50 +0000

    fix tests and update navbbar with cat

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,10 +2,16 @@ require "test_helper"
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
+  def setup
+   @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do 
+    #simulate user loggin without session hash using custon method  declared in test_helpers
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
-    assert_difference 'Category.count', 1 do
+    assert_difference 'Category.count', 1 do 
       post_via_redirect categories_path, category: {name: "sports"}
     end
     assert_template 'categories/index'
@@ -13,6 +19,8 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
    test "invalid category submission results in falure" do
+     #simulate user loggin without session hash using custon method declared in test_helpers
+    sign_in_as(@user, "password")
      get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
@@ -22,4 +30,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
   end
+  
+  
 end
